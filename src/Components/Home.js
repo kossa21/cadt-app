cf27e0b583b1adc69b436dffe452fb3d8207fefd
Anandamaya Arno <anandamaya.arno@free-now.com>
2021-04-28 00:26:18 +0200

    Cambiar el texto del home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Hello! welcome back!!!</div>;
+  return (
+    <div className="home">
+      <article>
+        <h1>Hello! Welcome back!!!</h1>
+        <h3>Navigate through the buttons to see the different tables</h3>
+      </article>
+    </div>
+  );
 };
 
 export default Home;
